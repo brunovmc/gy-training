0c4d9c0530d4e9020aac03c523cae8bcbfaffc72
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-01 14:57:53 -0300

    change sub to multiply

--- sub.js
+++ sub.js
@@ -1,9 +1,9 @@
 const userInput1 = prompt('Type a number');
 const userInput2 = prompt('Type another number');
 
-function sum(num1, num2){
-	let result = parseInt(num1) - parseInt(num2)
+function multiply(num1, num2){
+	let result = parseInt(num1) * parseInt(num2)
 	return result;
 }
 
-alert(`The subtraction of your numbers is ${sum(userInput1,userInput2)}`);
+alert(`The multiplication of your numbers is ${multiply(userInput1,userInput2)}`);
