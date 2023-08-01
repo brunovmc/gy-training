331cd25cbb197aa0145dc8f64f4d9ab64ec82d51
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-01 15:03:36 -0300

    add new multiplication method

--- sub.js
+++ sub.js
@@ -1,9 +1,9 @@
 const userInput1 = prompt('Type a number');
 const userInput2 = prompt('Type another number');
 
-function sum(num1, num2){
-	let result = parseInt(num1) - parseInt(num2)
+function multiplyThenAgainByHundred(num1, num2){
+	let result = (parseInt(num1) * parseInt(num2)) * 100;
 	return result;
 }
 
-alert(`The subtraction of your numbers is ${sum(userInput1,userInput2)}`);
+alert(`The product of your numbers multiplied by 100 is ${multiplyThenAgainByHundred(userInput1,userInput2)}`);
